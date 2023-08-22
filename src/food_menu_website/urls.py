6719038c73bea989fc205cc12e1579e4b9168b90
MuhammadMahdiAmirpour <muhammadmahdiamirpour@gmail.com>
2023-08-22 11:33:41 +0330

    adding the user profile picture plus its location

--- src/food_menu_website/urls.py
+++ src/food_menu_website/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path 
 from django.contrib.auth import views as authentication_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,8 @@ urlpatterns = [
     path('users/', include('users.urls'), name='users'),
     path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-]
+] 
+urlpatterns += [
+        # ... the rest of your url conf goes here ...
+        ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
