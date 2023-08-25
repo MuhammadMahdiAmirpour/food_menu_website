53fc323946f22cbfc5ae3f6fc67e8aab7cfe2a6c
MuhammadMahdiAmirpour <muhammadmahdiamirpour@gmail.com>
2023-08-25 21:08:18 +0330

    using class based views instead of function views

--- src/food_menu/urls.py
+++ src/food_menu/urls.py
@@ -3,13 +3,15 @@ from django.urls import path
 
 app_name = "food_menu"
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:item_id>/', views.detail, name='detail'),
+#     path('', views.index, name='index'),
+    path('', views.IndexClassView.as_view(), name='index'),
+#     path('<int:item_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.FoodDetail.as_view(), name='detail'),
     path('item/', views.Item, name='item'),
     # for adding items
-    path('add/', views.create_item, name='create_item'),
+    path('add/', views.CreateItem.as_view(), name='create_item'),
     # for editing items
-    path('edit/<int:id>', views.edit_item, name='edit_item'),
+    path('edit/<int:pk>', views.EditItem.as_view(), name='edit_item'),
     # for deleting items
-    path('delete/<int:item_id>', views.delete_item, name='delete_item'),
+    path('delete/<int:pk>', views.DeleteItem.as_view(), name='delete_item'),
 ]
--- src/food_menu/views.py
+++ src/food_menu/views.py
@@ -1,53 +1,90 @@
 from django.shortcuts import redirect
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from .models import Item
-from django.template import loader
+# from django.template import loader
 from .forms import ItemForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import DeleteView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 
-def index(request):
-    item_list = Item.objects.all()
-#     template = loader.get_template('food_menu/index.html')
-    context = {
-            'item_list': item_list,
-            }
-#    return HttpResponse(template.render(context, request))
-    return render(request, 'food_menu/index.html', context)
+# def index(request):
+#     item_list = Item.objects.all()
+# #     template = loader.get_template('food_menu/index.html')
+#     context = {
+#             'item_list': item_list,
+#             }
+# #    return HttpResponse(template.render(context, request))
+#     return render(request, 'food_menu/index.html', context)
+
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'food_menu/index.html'
+    context_object_name = 'item_list'
 
 # def item(request):
 #     return HttpResponse('<h1>This is an item view</h1>')
 
+class FoodDetail(DetailView):
+    model = Item
+    template_name = 'food_menu/detail.html'
 
-def detail(request, item_id):
-    item = Item.objects.get(pk=item_id)
-#    template = loader.get_template('food_menu/detail.html')
-    context = {
-            'item': item,
-            }
-#    return HttpResponse(template.render(context, request))
-    return render(request, 'food_menu/detail.html', context)
+# def detail(request, item_id):
+#     item = Item.objects.get(pk=item_id)
+# #    template = loader.get_template('food_menu/detail.html')
+#     context = {
+#             'item': item,
+#             }
+# #    return HttpResponse(template.render(context, request))
+#     return render(request, 'food_menu/detail.html', context)
 
-def create_item(request):
-    form = ItemForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect('food_menu:index')
-    return render(request, 'food_menu/item_form.html',{'form':form})
+class EditItem(UpdateView):
+    model = Item
+    fields = [
+            'item_name',
+            'item_description',
+            'item_price',
+            'item_image',
+            ]
+    success_url = 'food_menu:index'
 
-def edit_item(request, id):
-    item = Item.objects.get(id=id)
-    form = ItemForm(request.POST or None, instance = item)
-    if form.is_valid():
-        form.save()
-        return redirect('food_menu:index')
-    return render(request, 'food_menu/item_form.html', {'form':form, 'item': item})
+# def edit_item(request, id):
+#     item = Item.objects.get(id=id)
+#     form = ItemForm(request.POST or None, instance = item)
+#     if form.is_valid():
+#         form.save()
+#         return redirect('food_menu:index')
+#     return render(request, 'food_menu/item_form.html', {'form':form, 'item': item})
 
-def delete_item(request, item_id):
-    item = Item.objects.get(id=item_id)
-    if request.method == 'POST':
-        item.delete()
-        return redirect('food_menu:index')
-    return render(request, 'food_menu/item_delete.html', {'item': item})
+class CreateItem(CreateView):
+    model = Item
+    fields = [
+            'item_name',
+            'item_description',
+            'item_price',
+            'item_image',
+            ]
+
+# def create_item(request):
+#     form = ItemForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         return redirect('food_menu:index')
+#     return render(request, 'food_menu/item_form.html',{'form':form})
+
+class DeleteItem(DeleteView):
+    model = Item
+    success_url = 'food_menu:index'
+    template_name = 'food_menu/item_delete.html'
+
+# def delete_item(request, item_id):
+#     item = Item.objects.get(id=item_id)
+#     if request.method == 'POST':
+#         item.delete()
+#         return redirect('food_menu:index')
+#     return render(request, 'food_menu/item_delete.html', {'item': item})
 
